624e6c56e3d3e4c0fd3d7c053b0f5c2baad8280e
Sagar Subedi <sagarxic@gmail.com>
2024-01-21 21:40:50 +0545

    setup register module

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'mc-register',
+    templateUrl: './register.Component.html',
+    standalone: true,
+})
+export class RegisterComponent {
+
+}
