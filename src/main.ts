501f1d97c30dcb6768f93d98cf6bfe1c0e783fa3
Sagar Subedi <sagarxic@gmail.com>
2024-01-22 22:00:48 +0545

    setup dev-tools

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,19 @@ import {AppComponent} from './app/app.component'
 import {provideRouter} from '@angular/router'
 import {appRoutes} from './app/app.routes'
 import {provideStore} from '@ngrx/store'
+import {provideStoreDevtools} from '@ngrx/store-devtools'
+import {isDevMode} from '@angular/core'
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes), provideStore()],
+  providers: [
+    provideRouter(appRoutes),
+    provideStore(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+  ],
 })
