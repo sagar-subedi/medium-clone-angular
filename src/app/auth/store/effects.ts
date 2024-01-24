9b64b06789fd26e7e7519ad1e67ba83a346719f5
Subedi <sagar.subedi@cotiviti.com>
2024-01-24 13:19:09 +0545

    token persistance service added

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -2,17 +2,24 @@ import {inject} from '@angular/core'
 import {Actions, act, createEffect, ofType} from '@ngrx/effects'
 import {AuthService} from '../services/auth.service'
 import {authActions} from './actions'
-import {catchError, map, of, switchMap} from 'rxjs'
+import {catchError, map, of, switchMap, tap} from 'rxjs'
 import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface'
 import {HttpErrorResponse} from '@angular/common/http'
+import {PersistanceService} from 'src/app/shared/services/persistance.service'
+import {Router} from '@angular/router'
 
 export const registerEffects = createEffect(
-  (actions$ = inject(Actions), authService = inject(AuthService)) => {
+  (
+    actions$ = inject(Actions),
+    authService = inject(AuthService),
+    persistanceSerivce = inject(PersistanceService)
+  ) => {
     return actions$.pipe(
       ofType(authActions.register),
       switchMap(({request}) => {
         return authService.register(request).pipe(
           map((currentUser: CurrentUserInterface) => {
+            persistanceSerivce.set('accessToken', currentUser.token)
             return authActions.registerSuccess({currentUser})
           }),
           catchError((errorResponse: HttpErrorResponse) => {
@@ -26,5 +33,21 @@ export const registerEffects = createEffect(
   },
   {
     functional: true,
+  }
+)
+
+export const redirectAfterRegisterEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/')
+      })
+    )
+  },
+
+  {
+    functional: true,
+    dispatch: false,
   }
 )
