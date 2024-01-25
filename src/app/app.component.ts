7199890d9cdffa771dc8f654df11b7abb1745974
Subedi <sagar.subedi@cotiviti.com>
2024-01-25 14:32:51 +0545

    added topbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import {CommonModule} from '@angular/common'
 import {Component} from '@angular/core'
 import {RouterOutlet} from '@angular/router'
+import {TopBarComponent} from './shared/components/topBar/topBar.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent, CommonModule],
 })
 export class AppComponent {}
