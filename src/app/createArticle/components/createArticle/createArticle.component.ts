af7599e31264b30892eb95a1ecb92ab793df52be
Sagar Subedi <sagarxic@gmail.com>
2024-02-09 14:11:18 +0545

    added article creation form

--- src/app/createArticle/components/createArticle/createArticle.component.ts
+++ src/app/createArticle/components/createArticle/createArticle.component.ts
@@ -1,8 +1,22 @@
 import {Component} from '@angular/core'
+import {ArticleFormComponent} from 'src/app/shared/components/articleForm/articleForm.component'
+import {ArticleFormValuesInterface} from 'src/app/shared/types/articleFormValues.interface'
 
 @Component({
   selector: 'mc-create-article',
   templateUrl: './createArticle.component.html',
   standalone: true,
+  imports: [ArticleFormComponent],
 })
-export class CreateArticleComponent {}
+export class CreateArticleComponent {
+  initialValues = {
+    title: '',
+    description: '',
+    body: '',
+    taglist: [],
+  }
+
+  onSubmit(articleFormValues: ArticleFormValuesInterface) {
+    console.log('onsubmit in create article', articleFormValues)
+  }
+}
